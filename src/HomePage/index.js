0ad0458eb2752ffc13bc1302ab4618b30e20c79b
vitorbenedito <vitor.bsdc@gmail.com>
2021-10-27 19:07:15 -0300

    alterações header and footer + componente services

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './estilo_homepage.css';
 
 import EstruturaPagina from '../componentes/EstruturaPagina';
 
 const HomePage = () => {
     return (
         <EstruturaPagina>
-            <h1>Teste Page</h1>
+            <h1 id="teste-h1">Teste Page 1</h1>
+            <h1>Teste Page 2</h1>
+            <h1>Teste Page 3</h1>
+            <h1>Teste Page 4</h1>
+            <h1>Teste Page 5</h1>
+            <h1>Teste Page 6</h1>
+            <h1>Teste Page 7</h1>
+            <h1>Teste Page 8</h1>
         </EstruturaPagina>
     );
 }
--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import './estilo_header.css';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header-conteudo">
                 <nav className="header-logo-box">
-                    <img className="logo" src="" alt="logo-forza" title="logo-forza" />
-                    <h1 className="slogan">API | Forza</h1>
+                    <img className="logo" src="assets/icon-forza.png" alt="logo-forza" title="logo-forza" />
+                    <h1 className="slogan">API FORZA</h1>
                 </nav>
                 
                 <nav className="header-links">
-                    <a className="header-link" href="https://github.com/public-apis/public-apis#games-comics" target="_blank">Repositório API</a>
-                    <a className="header-link" href="https://docs.forza-api.tk" target="_blank">API Forza</a>
-                    <a className="header-link" href="https://github.com/nexusgroup6" target="_blank">GitHub Squad</a>
+                    <a className="header-link" href="https://github.com/public-apis/public-apis#games-comics" target="_blank">Repositório</a>
+                    <a className="header-link" href="https://docs.forza-api.tk" target="_blank">API</a>
+                    <a className="header-link" href="https://github.com/nexusgroup6" target="_blank">GitHub</a>
                 </nav>
             </div>
         </header>
